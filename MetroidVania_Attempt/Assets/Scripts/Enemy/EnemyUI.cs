9ee0ab97914766aa4fd9423078bf660db803afaf
KopeChris <chris_kopellis@hotmail.com>
2022-08-24 21:42:41 +0300

    sad

--- MetroidVania_Attempt/Assets/Scripts/Enemy/EnemyUI.cs
+++ MetroidVania_Attempt/Assets/Scripts/Enemy/EnemyUI.cs
@@ -12,7 +12,10 @@ public class EnemyUI : MonoBehaviour
     public float duration = 3f;
     public float countdown;
 
-    
+    private void Start()
+    {
+        textComponent.color = new Color(1, 0.5f, 0.5f, 0.8f);
+    }
     void Update()
     {
         //textComponent.text = enemy.playerDirectionX.ToString();   //to show playerdirection
@@ -20,16 +23,16 @@ public class EnemyUI : MonoBehaviour
 
         if(countdown > 0)
         {
-            textComponent.color = Color.red;
             textComponent.enabled = true;
             textComponent.text = damage.ToString();
-            countdown -= Time.deltaTime;
+            countdown -= Time.deltaTime + 0.001f;
         }
         
-        if(countdown <=0)
+        if(countdown <0)
         {
             textComponent.enabled = false;
             damage = 0;
+            countdown = 0;
         }
     }
 }
